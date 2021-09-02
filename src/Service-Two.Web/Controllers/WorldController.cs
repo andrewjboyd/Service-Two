01cd3299640611c9d7d83d9b674651ee82d6106c
Matt Goldman <matt.goldman@outlook.com>
2021-09-02 16:34:50 +1000

    created simple api, added tests

---
+++ src/Service-Two.Web/Controllers/WorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service_Two.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorldController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "World!";
+        }
+    }
+}
